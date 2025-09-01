aeba3d4cf965235ecb45777fa72e3c2d0d091eba
ZVDetector <linhai17181@gmail.com>
2025-09-02 02:47:15 +0800

    Update format.py

--- state_aware/format.py
+++ state_aware/format.py
@@ -2,11 +2,11 @@ import os
 import re
 import sys
 
-sys.path.append(os.path.dirname(os.getcwd()))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from util.utils import *
-from LLM import *
-from const import *
+from state_aware.LLM import *
+from state_aware.const import *
 
 
 def convert_name(name: str):
@@ -37,8 +37,8 @@ def convert_list_types(input_dict):
 
 class FormatGenerator:
     def __init__(self):
-        self.format_dir = os.path.join(os.getcwd(), "result/format")
-        self.description_dir = os.path.join(os.getcwd(), "result/description")
+        self.format_dir = os.path.join(os.path.dirname(__file__), "result/format")
+        self.description_dir = os.path.join(os.path.dirname(__file__), "result/description")
         self.LLM = LLMGenerator("(Your API Key)", "deepseek")
 
     def format_generation(self):
@@ -50,9 +50,101 @@ class FormatGenerator:
             frames = FRAMES[index]
             parts = PARTS[index]
 
+            with open(os.path.join(self.description_dir, f"{layer}.json"), "r") as f:
+                layer_descriptions = json.load(f)
+
             for frame, part in zip(frames, parts):
-                descript = descriptions[layer][frame]
-                message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+
+                if layer != "ZDP":
+                    general_frame_format_description = layer_descriptions[f"General {layer} Frame Format"]
+                else:
+                    general_frame_format_description = ""
+
+                if frame == "ACK":
+                    descript = {f"General {layer} Frame Format": general_frame_format_description,
+                                "Acknowledgement Frame Format": layer_descriptions["Format of Individual Frame Types"][
+                                    "Acknowledgement Frame Format"]}
+                    message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+
+                elif frame == "Data":
+                    descript = {f"General {layer} Frame Format": general_frame_format_description,
+                                "Acknowledgement Frame Format": layer_descriptions["Format of Individual Frame Types"][
+                                    "Data Frame Format"]}
+                    message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+
+                elif frame == "Beacon":
+                    descript = {f"General {layer} Frame Format": general_frame_format_description,
+                                "Beacon Frame Format": layer_descriptions["Format of Individual Frame Types"][
+                                    "Beacon Frame Format"]}
+
+                    message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+
+                else:
+                    message_format = {}
+
+                    if layer != "ZDP":
+                        base_descript = {f"General {layer} Frame Format": general_frame_format_description,
+                                         "Command Frame Format": layer_descriptions["Format of Individual Frame Types"]
+                                         [f"{layer} Command Frame Format"]}
+                        all_commands = layer_descriptions["Command Frames"]
+                    else:
+                        base_descript = {}
+                        all_commands = {}
+
+                    if layer == "APS":
+                        pattern = f"{layer}_CMD_"
+
+                        for cname in all_commands["Command Identifier"]:
+                            if cname == "Reserved":
+                                continue
+
+                            if cname.startswith(pattern):
+                                cname = cname[len(pattern):]
+                            else:
+                                continue
+
+                            cmd_formatted = cname.title().replace("_", "-")
+                            descript_key = f"{cmd_formatted} Commands"
+
+                            descript = base_descript | {f"{descript_key}": layer_descriptions[descript_key]}
+
+                            single_message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+
+                            message_format.update(single_message_format)
+
+                    elif layer == "NWK":
+                        for cname, cmd_descript in all_commands.items():
+
+                            cmd_final_name = cname.replace(" ", "")
+                            descript = base_descript | {f"{cmd_final_name}": cmd_descript}
+
+                            single_message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+                            message_format.update(single_message_format)
+
+                    elif layer == "MAC":
+                        for index2, cname in enumerate(all_commands["Command Identifier"]):
+                            if cname == "Reserved":
+                                continue
+
+                            cmd_id = all_commands["Value"][index2]
+                            cmd_final_name = cname.replace(" ", "")
+
+                            descript = base_descript | {f"Command Name": cmd_final_name, f"Command Identifier": cmd_id}
+                            single_message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+                            message_format.update(single_message_format)
+
+                    else:
+                        zdp_cmd_type = ["client", "server"]
+                        for cmd_type in zdp_cmd_type:
+
+                            type_commands = layer_descriptions[cmd_type]
+                            commands = type_commands["messages"]
+
+                            for command_type in commands.keys():
+                                for command in commands[command_type]:
+                                    descript = {f"{command}": type_commands[command]}
+                                    single_message_format = self.LLM.LLM_format_generation(layer, frame, part, descript)
+                                    message_format.update(single_message_format)
 
                 with open(os.path.join(self.format_dir, "{}/llm_format({}_{}).json".format(layer, layer, frame)), "w") as f:
                     json.dump(message_format, f, indent=4)
@@ -93,8 +185,11 @@ class FormatGenerator:
             new_result[cmd_name] = new_argument
 
         new_result = convert_list_types(new_result)
-        with open(os.path.join(self.format_dir, "{}/format({}_{}).json".format(layer, layer, frame)), "w") as f:
-            json.dump(new_result, f, indent=4)
+        save_path = os.path.join(self.format_dir, "{}/format({}_{}).json".format(layer, layer, frame))
+
+        if not os.path.exists(save_path):
+            with open(save_path, "w") as f:
+                json.dump(new_result, f, indent=4)
 
     def merge_all_messages(self):
         all_messages = {}
@@ -103,6 +198,8 @@ class FormatGenerator:
         total_count = 0
         nozcl_count = 0
         general_count = 0
+
+        save_path = os.path.join(self.format_dir, "all_formats(Zigbee).json")
 
         for index, layer in enumerate(LAYERS):
             layer_messages = {}
@@ -130,20 +227,28 @@ class FormatGenerator:
             total_count += len(layer_messages)
             all_messages[layer] = layer_messages
 
-        with open(os.path.join(self.format_dir, "all_formats(Zigbee).json"), "w") as f:
-            json.dump(all_messages, f, indent=4)
+        if not os.path.exists(save_path):
+            with open(save_path, "w") as f:
+                json.dump(all_messages, f, indent=4)
 
-        print(f"ZCL Other Layers Message Count: {nozcl_count}")
-        print(f"ZCL General Message Count: {general_count}")
-        print(f"ZCL Command Count: {len(zcl_messages)}")
-        print(f"Extracted message count: {total_count}")
+        log.info(f"ZCL Other Layers Message Count: {nozcl_count}")
+        log.info(f"ZCL General Message Count: {general_count}")
+        log.info(f"ZCL Command Count: {len(zcl_messages)}")
+        log.info(f"Extracted message count: {total_count}")
+
+    async def run(self, format_generated=False):
+        log.info(f"[Protocol State Awareness] Starting Generating Zigbee Formats...")
+
+        if not format_generated:
+            self.format_generation()
+        time.sleep(5)
+
+        self.merge_all_messages()
+        time.sleep(5)
+
+        log.info(f"[Protocol State Awareness] Generating Zigbee Formats Done!")
 
 
 if __name__ == "__main__":
     fg = FormatGenerator()
-
-    # 1. Generate the format
-    # fg.format_generation()
-
-    # 2. Summary all messages into a json file
-    fg.merge_all_messages()
+    fg.run()
